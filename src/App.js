a40dc33b4d8aaa4cf748baa831a0775c8f79b48a
unknown <ajanthan.dev@hotelshippo.lk>
2023-03-23 21:50:41 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import { Login } from "./login/index";
+import { 
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Routes,
+  Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        < Login usernameper="Book" />
-        </header>
-    </div>
-    /*
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> 
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            
+          </Routes>
+        </Router>
       </header>
     </div>
-    */
   );
 }
 
